5bc5dad974e666c9f3e23c27c3548f6a85b2e27c
John Goh <johngohrw@gmail.com>
2023-04-22 14:30:30 +0800

    feat: GET /test API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,15 @@ const APP_PORT_NUMBER = 3000
 
 app.use(express.json())
 
+// Hello World get request
+app.get('/test', async (req, res) => {
+    const result = {
+        Hello: "world"
+    }
+    res.json(result)
+})
+
+
 const server = app.listen(APP_PORT_NUMBER, () =>
     console.log(`Server up at: http://localhost:${APP_PORT_NUMBER}`),
 )
