e19064ad9f8ff6b9aabcd6917e4d126435a11240
John Goh <johngohrw@gmail.com>
2023-04-22 15:35:10 +0800

    feat: create column, create task, delete task APIs

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -36,6 +36,52 @@ app.get('/columns', async (req, res) => {
     res.json(columns)
 })
 
+// create new column for a user
+app.post(`/column`, async (req, res) => {
+    const { title, userId } = req.body
+    const result = await prisma.column.create({
+        data: {
+            title,
+            user: { connect: { id: userId } },
+        },
+    })
+    res.json(result)
+})
+
+
+// create new task for a particular column
+app.post(`/task`, async (req, res) => {
+    const { title, description, columnId } = req.body
+    try {
+        const result = await prisma.task.create({
+            data: {
+                title,
+                description,
+                column: { connect: { id: columnId } },
+            },
+        })
+        res.json(result)
+    } catch (error) {
+        res.json({ error })
+    }
+})
+
+
+// delete a task
+app.delete(`/task/:id`, async (req, res) => {
+    const { id } = req.params
+    try {
+        const task = await prisma.task.delete({
+            where: {
+                id: Number(id),
+            },
+        })
+        res.json(task)
+    } catch (error) {
+        res.json({ error: `task with ID ${id} does not exist in the database.` })
+    }
+})
+
 const server = app.listen(APP_PORT_NUMBER, () =>
     console.log(`Server up at: http://localhost:${APP_PORT_NUMBER}`),
 )
